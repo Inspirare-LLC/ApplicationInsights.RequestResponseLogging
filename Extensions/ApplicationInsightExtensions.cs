598dcb797248b50ab0f3a83f85950e70f117b8d0
Jonas Petraška <jonas.petraska@gmail.com>
2021-08-16 10:59:33 +0300

    Initial

---
+++ Extensions/ApplicationInsightExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationInsights.RequestResponseLogging.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationInsights.RequestResponseLogging.Extensions
+{
+    /// <summary>
+    /// Extensions for enabling request or response logging
+    /// </summary>
+    public static class ApplicationInsightExtensions
+    {
+        public static IApplicationBuilder UseRequestBodyLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggingMiddleware>();
+        }
+
+        public static IApplicationBuilder UseResponseBodyLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseLoggingMiddleware>();
+        }
+    }
+}
---
+++ Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInsights.RequestResponseLogging.Middlewares
+{
+    /// <summary>
+    /// Middleware for logging request body and query string
+    /// </summary>
+    public class RequestLoggingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var method = context.Request.Method;
+
+            // Ensure the request body can be read multiple times
+            context.Request.EnableBuffering();
+
+            // Only if we are dealing with POST or PUT, GET and others shouldn't have a body
+            if (context.Request.Body.CanRead && (method == HttpMethods.Post || method == HttpMethods.Put))
+            {
+                // Leave stream open so next middleware can read it
+                using var reader = new StreamReader(
+                    context.Request.Body,
+                    Encoding.UTF8,
+                    detectEncodingFromByteOrderMarks: false,
+                    bufferSize: 512, leaveOpen: true);
+
+                var requestBody = await reader.ReadToEndAsync();
+
+                // Reset stream position, so next middleware can read it
+                context.Request.Body.Position = 0;
+
+                // Write request body to App Insights
+                var requestTelemetry = context.Features.Get<RequestTelemetry>();
+                requestTelemetry?.Properties.Add("RequestBody", requestBody);
+            }
+
+            // Additionaly log query string
+            if (context.Request.QueryString != null && 
+                !String.IsNullOrEmpty(context.Request.QueryString.ToString()))
+            {
+                var requestTelemetry = context.Features.Get<RequestTelemetry>();
+                requestTelemetry?.Properties.Add("RequestQueryString", context.Request.QueryString.ToString());
+            }
+
+            // Call next middleware in the pipeline
+            await next(context);
+        }
+    }
+}
